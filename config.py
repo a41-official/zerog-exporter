b41c86c8f46628be3244d8b016e407658e526bf5
steve.kim <steve.kim@a41.io>
2025-08-07 22:31:44 +0900

    update validator config

--- config.py
+++ config.py
@@ -12,12 +12,16 @@ class Config:
     EXPORTER_PORT = int(os.getenv('EXPORTER_PORT', '26660'))
     
     TOKEN_DENOM = os.getenv('TOKEN_DENOM', 'u0g')
-    TOKEN_COEFFICIENT = int(os.getenv('TOKEN_COEFFICIENT', '1000000'))
+    TOKEN_COEFFICIENT = int(os.getenv('TOKEN_COEFFICIENT', '1000000000'))
     TOTAL_SUPPLY = int(os.getenv('TOTAL_SUPPLY', '1000000000'))
+    
+
     
     METRICS_SCRAPE_INTERVAL = int(os.getenv('METRICS_SCRAPE_INTERVAL', '15'))
     METRICS_TIMEOUT = int(os.getenv('METRICS_TIMEOUT', '10'))
     METRICS_RETRY_COUNT = int(os.getenv('METRICS_RETRY_COUNT', '3'))
+    
+    VALIDATOR_ADDRESS = os.getenv('VALIDATOR_ADDRESS', '30535EF0D596876C5DBFCF825D64134550AB4945')
     
     LOG_LEVEL = os.getenv('LOG_LEVEL', 'info')
     LOG_FORMAT = os.getenv('LOG_FORMAT', 'json')
